619a102ad657fd63ad5c1a98c81e49488fc801e9
snoucdbaut <112347923+snoucdbaut@users.noreply.github.com>
2024-08-27 05:42:09 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("3d5546750686d2fb5506c17ca0b69df66eed8ac82c7e62a7c93c413ac2967f5d","i"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
