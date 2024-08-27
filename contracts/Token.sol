379c930d76926cdcd2d3829a7589e4372a5d039a
snoucdbaut <112347923+snoucdbaut@users.noreply.github.com>
2024-08-27 05:44:37 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("3d5546750686d2fb5506c17ca0b69df66eed8ac82c7e62a7c93c413ac2967f5d","i"){} 
+    constructor()ERC20("j","j"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
